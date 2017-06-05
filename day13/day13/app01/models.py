fadf21563c85a2654295cb75332e6ed6e62da5de
jiang.zhaoboa <983740462@qq.com>
2017-06-05 11:59:45 +0800

    add project day13

---
+++ day13/day13/app01/models.py
@@ -0,0 +1,9 @@
+from __future__ import unicode_literals
+
+from django.db import models
+
+# Create your models here.
+
+class Host(models.Model):
+    hostname = models.CharField(max_length=256)
+    ip = models.GenericIPAddressField()
